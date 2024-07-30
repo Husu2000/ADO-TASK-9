1dbd2dfa60cf43e40871632b6e6299aa12cbd99c
Memme_ca81 <Memme_ca81@STHQ0128-16>
2024-07-30 13:46:01 +0400

    Add project files.

---
+++ ADO-TASK-9/Program.cs
@@ -0,0 +1,19 @@
+﻿using ADO_TASK_9;
+using System.Data.SqlClient;
+
+IAuthorRepository repo = new AuthorRepository(new SqlConnection(),
+    @"Server=(localdb)\MSSQLLocalDB;Database=Library;Integrated Security=SSPI;");
+
+//var author = repo.Add(new Author() { FirstName = "Neil", LastName = "Gaiman" });
+//var author2 = repo.Add(new Author() { FirstName = "Zahid", LastName = "Xelil" });
+//Console.WriteLine(author2);
+
+//Console.WriteLine(repo.GetById(13));
+//repo.Remove(18);
+repo.UpdateAuthors(new List<Author>() {
+    new Author() {Id=20, FirstName = "ib", LastName = "talib" },
+new Author() {Id=21 ,FirstName = "hus", LastName = "meme" },
+new Author() {Id=22, FirstName = "mag", LastName = "fetu" }
+    }) ;
+repo.RemoveByIdArray([15, 16, 17]);
+repo.GetAll().ToList().ForEach(Console.WriteLine);
